5a208661e12797f0095c589a8a3704500a71a482
gabrielsacagami <gabrieldnzs@gmail.com>
2022-10-10 02:44:17 -0300

    Clean tests

--- requestMain.py
+++ requestMain.py
@@ -1,29 +1,22 @@
 import requests
 import json
 
-echo_get = 'https://postman-echo.com/get'
-echo_post = 'https://postman-echo.com/post'
+url = 'https://postman-echo.com/post'
 
 with open("body.json", "r") as read_file:
     my_body = json.load(read_file)
 
-print(my_body["address"]["line1"])
-my_body["param_2"] = 'new_var_2'
-my_body["address"]["line1"] = 'New Street'
-
+# header params
 my_header = dict(
     account = 'acc_xyz',
     merchant = 'merch_abc'
 ) 
 
-my_query_string = dict(
-    foo1='bar1'
-)
+# manipulate json body
+my_body["param_2"] = 'new_var_2'
+my_body["address"]["line1"] = 'New Street'
 
-# get_response = requests.get(url=echo_get, params=my_query_string, headers=my_header)
-
-post_response = requests.post(url=echo_post, params=my_query_string, headers=my_header, json=my_body)
-
+post_response = requests.post(url=url, headers=my_header, json=my_body)
 response_status = f'Response Status: {post_response.status_code}'
 response = post_response.json()
 
