58be758a5d2abe2633f0459394586eaee8b8e8bc
gabrielsacagami <gabrieldnzs@gmail.com>
2022-10-12 03:34:29 -0300

    Add CSV write capabilities

--- requestMain.py
+++ requestMain.py
@@ -1,39 +1,47 @@
 import requests
 import json
 import csv
+import time
 
 url = 'https://postman-echo.com/post'
 
 with open("body.json", "r") as read_file:
     my_body = json.load(read_file)
 
-with open('data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(row[4])
-            print(row[1])
-            print(row[8])
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-
 # header params
 my_header = dict(
     account = 'acc_xyz',
     merchant = 'merch_abc'
-) 
+)
 
-# manipulate json body
-my_body["criadaPor"] = 'python_runner'
-my_body["terminal"]["id"] = 'NOVO_ID'
+with open('response.csv', mode='w') as response_file:
+    response_writer = csv.writer(response_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    response_writer.writerow(['cep', 'id_meio_captura', 'status'])
 
-post_response = requests.post(url=url, headers=my_header, json=my_body)
-response_status = f'Response Status: {post_response.status_code}'
-response = post_response.json()
+    with open('data.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
 
-print(response)
-print(response_status)
+            # manipulate json body
+            my_body["cep"] = row["cep"]
+            my_body["id_meio_captura"] = row["id_meio_captura"]
+
+            post_response = requests.post(url=url, headers=my_header, json=my_body)
+            response_status = f'Response Status: {post_response.status_code}'
+            response = post_response.json()
+
+            response_writer.writerow([row["cep"], row["id_meio_captura"], response_status])
+
+            print(response)
+            print(response_status)
+
+            line_count += 1
+            time.sleep(1)
+
+    print(f'Processed {line_count} lines.')
+
+
